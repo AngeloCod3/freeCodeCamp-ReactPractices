9373def18cf9e0c7c910fdcfc3d662d4cb5cc6f8
Angelo <angelodeoliveiramix@gmail.com>
2022-07-14 13:08:08 -0400

    Almost finish, 95% similar to the original testimonies component, include responsive design

--- freecodecamp-clone/src/components/testimonies.jsx
+++ freecodecamp-clone/src/components/testimonies.jsx
@@ -1,19 +1,24 @@
-import React from "react";
+
 import "../styles/testimonies.css"
-function Testimonios() {
+
+function Testimonios(props) {
   return (
+   
     <div className="main-container">
-      <img src={require("../images/Emma.png")}alt="Emma Photo" className="testimonies-images" />
+      <img
+       src= {require(`../images/${props.imagen}.png`)} alt={props.alternative}
+       className="testimonies-images" />
 
       <div className="testimonies-text">
-        <h3 className="TitleName">Emma Bostian in Sweden</h3>
-        <h3 className="Carrer">Software Engineer at Spotify</h3>
+        <p className="TitleName"><strong> {props.name} </strong> in {props.country}</p>
+        <p className="Carrer">{props.carreer} at <strong>{props.company}</strong></p>
         <p className="description-text">
-        "I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
+        "{props.testimony}"
         </p>
       </div>
     </div>
   );
 }
+
 
 export default Testimonios;
