79b731560802fc85ef681c53d9dcfb512e5fe81a
Angelo <angelodeoliveiramix@gmail.com>
2022-07-26 20:27:48 -0400

    Setting State on React Game Tictoctoe

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Tarea from './Tarea';
 import './App.css';
 
 function App() {
@@ -7,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Tarea name='Angelo'/>
+          
         </p>
         <a
           className="App-link"
